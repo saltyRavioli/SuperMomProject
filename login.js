2f638833834eae84b25131eddabf092538240d11
Nathan Lu <56777867+saltyRavioli@users.noreply.github.com>
2020-07-02 15:13:13 -0400

    Update

--- login.js
+++ login.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var mysql = require('mysql');
 var session = require('client-sessions'); 
- 
+var nodemailer = require('nodemailer');
+var uuid = require('node-uuid');
+var url = require('url');
+
 //use the application off of express.
 var app = express();
 
@@ -11,8 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // need this to do stuff lik
 app.use(session({
   cookieName: 'SuperMomSession', // cookie name dictates the key name added to the request object
   secret: 'random_string_goes_here', //encrypt later (should be a large unguessable string)
-  duration: 30 * 60 * 1000, // how long the session will stay valid in ms
-  activeDuration: 5 * 60 * 1000, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+  duration: 1000*60*60*3, // how long the session will stay valid in ms
   cookie: { //https://www.npmjs.com/package/client-sessions
     //path: '/api', // cookie will only be sent to requests under '/api'
     maxAge: 1000*60*60*24*3, // duration of the cookie in milliseconds, defaults to 60000
@@ -43,12 +45,65 @@ con.connect(function(err) {
 
 //define the route for "/"
 app.get("/login", function (request, response) { //apparently app.get() only activates if the user types in the password?
+  if (request.SuperMomSession.remember == true) {
+    response.render('login', {
+          username: request.SuperMomSession.user,
+          password: request.SuperMomSession.pword
+    })
+  }
     //show this file when the "/" is requested
-    response.sendFile(__dirname+"/login.html");
+  else {
+    response.render('login', {
+      username: '',
+      password: ''
+    })
+  }
 });
-app.post("/login", function (request, response) { //apparently app.post() only activates if the page is called by another page?`
+app.post("/login", function (request, response) {
+  if (request.SuperMomSession.remember == true) {
+    response.render('login', {
+          username: request.SuperMomSession.uname,
+          password: request.SuperMomSession.pword
+    })
+  }
   //show this file when the "/" is requested
-  response.sendFile(__dirname+"/login.html");
+  else {
+    response.render('login', {
+      username: '',
+      password: ''
+    })
+  }
+});
+
+app.post("/index", function (request, response) {
+  if (request.SuperMomSession.remember == true) {
+    response.render('index', {
+          username: request.SuperMomSession.uname,
+          password: request.SuperMomSession.pword
+    })
+  }
+  //show this file when the "/" is requested
+  else {
+    response.render('index', {
+      username: '',
+      password: ''
+    })
+  }
+});
+app.get("/index", function (request, response) {
+  if (request.SuperMomSession.remember == true) {
+    response.render('index', {
+          username: request.SuperMomSession.uname,
+          password: request.SuperMomSession.pword
+    })
+  }
+  //show this file when the "/" is requested
+  else {
+    response.render('index', {
+      username: '',
+      password: ''
+    })
+  }
 });
 
 /*
@@ -68,10 +123,6 @@ app.get("/signup", function (req, res) {
 app.get("/homepage", function (req, res) {
   console.log(req.SuperMomSession);
   let sql = `SELECT * FROM members WHERE user_name = '${req.SuperMomSession.user}'`;
-  console.log('Username:')
-  console.log(req.SuperMomSession.user);
-  console.log('Password:')
-  console.log(req.SuperMomSession.pword);
   let query = con.query(sql, function(err, result) {
     if(err) {
         console.log("An error occurred.");
@@ -119,6 +170,7 @@ app.post("/signupCheck", function (req, res) {
 );
 
 app.post('/validLogin', (req, res) => {
+    console.log(req.body.uname);
     let sql = `SELECT * FROM members WHERE user_name = '${req.body.uname}'`; //back text not apostrophe
     //also req.body.uname is used to get input from html
     let query = con.query(sql, function(err, result) {
@@ -135,6 +187,13 @@ app.post('/validLogin', (req, res) => {
             req.SuperMomSession.user = result[0].user_name;
             req.SuperMomSession.pword = result[0].pword;
             req.SuperMomSession.memberID = result[0].memberID;
+            if (req.body.remember=='on') {
+              console.log(req.body.remember);
+              req.SuperMomSession.remember = true;
+            }
+            else {
+              req.SuperMomSession.remember = false;
+            }
             res.sendFile(__dirname+"/homepage.html");
           }
           else {
@@ -146,6 +205,95 @@ app.post('/validLogin', (req, res) => {
           console.log(result[0].pword);
         }
     });
+});
+
+app.get("/enterEmail", function (req, res) {
+  res.sendFile(__dirname+"/enterEmail.html");
+});
+
+app.post("/forgotPassword", function (req, res) {
+  console.log(req.body.email);
+  let sql = `SELECT * FROM members WHERE email = '${req.body.email}'`;
+  let query = con.query(sql, function(err, result) {
+    console.log(result);
+    if(err) {
+        console.log("An error occurred.");
+        throw err;
+    }
+    else if (result.length == 0) {
+      res.sendFile(__dirname+"/loginFailed.html");
+    }
+    else {
+      req.SuperMomSession.user = result[0].user_name;
+      req.SuperMomSession.pword = result[0].pword;
+      req.SuperMomSession.memberID = result[0].memberID;
+      console.log(req.SuperMomSession.memberID);
+    }
+  });
+
+  var token = uuid.v1();
+  console.log(token);
+
+  var transporter = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+          user: "d2c057524d03ff",
+          pass: "44031373928ff5"
+      }
+  });
+  
+  var sql2 = `UPDATE members SET 
+          resetPasswordKey = '${token}', 
+          expires = ADDTIME(CURRENT_TIMESTAMP(), "2:00:00")
+        WHERE memberID = ${req.SuperMomSession.memberID};`
+  let query2 = con.query(sql2, function(err, result) {
+    if(err) {
+        console.log("An error occurred.");
+        throw err;
+    }
+    else {
+      console.log(result);
+    }
+  });
+  
+  var mailOptions = {
+      from: 'nathanymlu@gmail.com',
+      to: 'nathanymlu@gmail.com',
+      subject: 'Sending Email using Node.js',
+      text: 'Reset your password by clicking on the following link: '+__dirname+"/resetPassword/?token="+token
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+          console.log(error);
+      } else {
+          console.log('Email sent: ' + info.response);
+      }
+  });
+  res.sendFile(__dirname+"/emailSent.html");
+});
+
+app.get('/resetPassword*', function (req,res) {
+  var adr = req.url;
+  var q = url.parse(adr, true);
+  var qdata = q.query;
+  var token = qdata.token; 
+  let sql = `SELECT * FROM members WHERE resetPasswordKey = '${token}'`;
+  let query = con.query(sql, function(err, result) {
+    if(err) {
+        console.log("An error occurred.");
+        throw err;
+    }
+    else if (result.length == 0 || Date.parse(result[0].expires)<Date.now()) {
+      res.sendFile(__dirname+"/loginFailed.html");
+    }
+    else {
+      req.SuperMomSession.user = result[0].user_name;
+      req.SuperMomSession.pword = result[0].pword;
+      req.SuperMomSession.memberID = result[0].memberID;
+      res.sendFile(__dirname+"/resetPassword.html");
+    }
+  });
 });
 
 app.get('/members', function (req,res) {
@@ -163,6 +311,26 @@ app.get('/members', function (req,res) {
       })
   });
 });
+
+app.post("/newPassword", function (req, res) {
+  let sql = `UPDATE members SET 
+  pword = '${req.body.pword}' 
+  WHERE memberID = ${req.SuperMomSession.memberID};`;
+    let query = con.query(sql, function(err, result) {
+      if(err) {
+          console.log("An error occurred.");
+          throw err;
+      }
+      else {
+        console.log(result);
+        res.render('login', {
+          username: '',
+          password: ''
+        })
+      }
+    }); 
+  }
+);
 
 function checkAuth(req, res, next) {
   if (!req.session.user_id) {
@@ -220,16 +388,10 @@ app.get('/createOrders', (req, res) => {
     }
   });
 });
-app.get('/createOrders', (req, res) => {
-  sql = `create table orders(
-    order_id bigint primary key auto_increment,
-    product_id bigint not null,
-    member_id bigint not null,
-    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    ratings int,
-    feedback text(1000),
-    order_status varchar(30)
-    );`
+app.get('/modifyMembers', (req, res) => {
+  sql = `ALTER TABLE members
+  ADD COLUMN resetPasswordKey VARCHAR(255) AFTER level_id,
+  ADD COLUMN expires DATETIME AFTER resetPasswordKey`
   let query = con.query(sql, function(err, result) {
     if(err) {
         console.log("An error occurred.");
@@ -293,24 +455,43 @@ app.get('/addProducts', (req, res) => {
     }
   });
 });
+app.get('/createChats', (req, res) => {
+  sql = `create table chats(
+    chat_id bigint primary key auto_increment,
+    room_id int not null,
+    member_id bigint not null,
+    chat_content text(1000),
+    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );`
+  let query = con.query(sql, function(err, result) {
+    if(err) {
+        console.log("An error occurred.");
+        throw err;
+    }
+    else {
+      console.log(result);
+    }
+  });
+});
+app.get('/createChatrooms', (req, res) => {
+  sql = `create table chatroom(
+    room_id int primary key auto_increment,
+    room_title varchar (50) not null,
+    topic_id int not null,
+    authority varchar(30) not null
+    );`
+  let query = con.query(sql, function(err, result) {
+    if(err) {
+        console.log("An error occurred.");
+        throw err;
+    }
+    else {
+      console.log(result);
+    }
+  });
+});
 
 app.get('/orders', (req, res) => {
-  // let sql = `SELECT * FROM products WHERE product_id = ANY 
-  //       (SELECT * FROM orders WHERE member_id = ANY 
-  //         (SELECT * FROM members WHERE user_name = '${req.SuperMomSession.user}'
-  //       ));`
-  //       //sql subqueries
-  // let query = con.query(sql, function(err, rows, result) {
-  //   if(err) {
-  //       console.log("An error occurred.");
-  //       throw err;
-  //   }
-  //   res.render('orders', {
-  //       title: 'Order List',
-  //       items: rows
-  //   })
-  //   console.log(rows);
-  // });
   let sql = `SELECT * FROM members WHERE user_name = '${req.SuperMomSession.user}'`; 
   let query = con.query(sql, function(err, result) {
     if(err) {
@@ -325,53 +506,31 @@ app.get('/orders', (req, res) => {
       console.log("test: "+result);
       var member_ID = -1; 
       member_ID = result[0].memberID;
-      let sql2 = `SELECT * FROM products WHERE product_id IN (SELECT product_id FROM orders WHERE member_id = '${member_ID}');`; //yay subqueries
+      let sql2 = ` SELECT products.product_id, product_name, product_description, product_price FROM products 
+      INNER JOIN orders
+      ON (products.product_id = orders.product_id)
+      WHERE member_id = '${member_ID}';
+      `; //yay inner join
+      
 
       let query2 = con.query(sql2, function(err, rows, fields) {
           if(err) {
               console.log("An error occurred.");
               throw err;
           }
-          // res.write('<h1>Hello, World!</h1>');
-          // res.end();
-          res.render('orders', { // 'posts' <-- whatever's in here should match the filename in the views folder
+          var totalPrice = 0;
+          rows.forEach((item, index) => { //for each loop
+            totalPrice+=item.product_price;
+          });
+          totalPrice = totalPrice.toFixed(2); //rounding
+          res.render('orders', {
               title: 'Product Details',
-              items: rows
+              items: rows,
+              price: totalPrice 
           })
       });
     }
   });
-/*
-  let sql = `SELECT * FROM members WHERE user_name = '${req.SuperMomSession.user}'`; 
-  let query = con.query(sql, function(err, result) {
-    if(err) {
-        console.log("An error occurred.");
-        throw err;
-    }
-    else if (result.length == 0) {
-        console.log("No entry found:" + result);
-        res.sendFile(__dirname+"/Error.html");
-    }
-    else {
-      var member_ID = -1; 
-      member_ID = result[0].memberID;
-      let sql2 = `SELECT * FROM orders WHERE member_id = '${member_ID}'`; 
-      let sql3 = `SELECT * FROM products WHERE product_id = ANY 
-        (SELECT * FROM orders WHERE member_id = ANY 
-          (SELECT * FROM members WHERE user_name = '${req.SuperMomSession.user}'
-        ));`
-      let query2 = con.query(sql2, function(err, rows, fields) {
-        if(err) {
-            console.log("An error occurred.");
-            throw err;
-        }
-        res.render('orders', { // 'posts' <-- whatever's in here should match the filename in the views folder
-            title: 'Order List',
-            items: rows
-        })
-      });
-    }
-*/
 });
 app.get('/products', (req, res) => {
   let sql = `SELECT * FROM products`;
@@ -381,12 +540,21 @@ app.get('/products', (req, res) => {
           console.log("An error occurred.");
           throw err;
       }
-      // res.write('<h1>Hello, World!</h1>');
-      // res.end();
-      res.render('orders', { // 'posts' <-- whatever's in here should match the filename in the views folder
-          title: 'Product Details',
-          items: rows
-      })
+      let sql2 = `SELECT * FROM category`;
+      let query2 = con.query(sql2, function(err, rows2, fields) {
+          if(err) {
+              console.log("An error occurred.");
+              throw err;
+          }
+          console.log(rows2);
+          // res.write('<h1>Hello, World!</h1>');
+          // res.end();
+          res.render('orders', { // 'posts' <-- whatever's in here should match the filename in the views folder
+              title: 'Product Details',
+              items: rows,
+              categories: rows2
+          })
+      });
   });
 });
 
@@ -412,6 +580,89 @@ app.post('/addToOrder', (req, res) => {
           items: rows
       })
   });
+});
+
+app.post('/searchProducts', (req, res) => {
+  console.log(req.body.category);
+  let sql = `SELECT * FROM products WHERE category = '${req.body.category}'`;
+  let query = con.query(sql, function(err, rows, fields) {
+      if(err) {
+          console.log("An error occurred.");
+          throw err;
+      }
+      let sql2 = `SELECT * FROM category`;
+      let query2 = con.query(sql2, function(err, rows2, fields) {
+        if(err) {
+            console.log("An error occurred.");
+            throw err;
+        }
+        res.render('orders', { // 'posts' <-- whatever's in here should match the filename in the views folder
+            title: 'Product Details',
+            items: rows,
+            categories: rows2
+        })
+    });
+  });
+});
+
+app.post("/adminPageCheck", function (req, res) {
+  console.log(req.body.topic);
+  if (req.body.topic != undefined) {
+    sql = `INSERT INTO topic (topic_name) values ('${req.body.topic}')`
+    let query = con.query(sql, function(err, result) {
+      if(err) {
+          console.log("An error occurred.");
+          throw err;
+      }
+      else {
+      }
+    });
+  }
+  console.log(req.body.category);
+  if (req.body.category != undefined) {
+    sql = `INSERT INTO category (topic_name) values ('${req.body.category}')`
+    let query = con.query(sql, function(err, result) {
+      if(err) {
+          console.log("An error occurred.");
+          throw err;
+      }
+      else {
+        
+      }
+    });
+  }
+  res.sendFile(__dirname+"/adminPage.html");
+});
+app.get('/adminPageTableCreate', (req, res) => {
+  /*
+  create table topic (
+    topic_id int primary key auto_increment,
+    topic_name varchar(100),
+    topic_desc text(1000)
+  );
+  */
+  sql = `   
+    create table category (
+      topic_id int primary key auto_increment,
+      topic_name varchar(100),
+      topic_desc text(1000)
+    );`
+  let query = con.query(sql, function(err, result) {
+    if(err) {
+        console.log("An error occurred.");
+        throw err;
+    }
+    else {
+      console.log(result);
+    }
+  });
+});
+
+app.get("/adminPage", function (request, response) {
+  response.sendFile(__dirname+"/adminPage.html");
+});
+app.post("/adminPage", function (request, response) {
+    response.sendFile(__dirname+"/adminPage.html");
 });
 
 app.get('/logout', function (req, res) {
